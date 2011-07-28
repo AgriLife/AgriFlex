64b24efa37a59c8b29cf33ba31602a855ffd142b
Travis Ward <travis@travisward.com>
2011-07-28 14:57:23 -0500

    adding fields to footer

--- footer.php
+++ footer.php
@@ -125,16 +125,31 @@
 		<div id="contact">
 			<div class="contact">			
 			<h4>Contact</h4>
-				<?php $mapaddress=$options['address-street1'].' '.$options['address-street2'].' '.$options['address-city'].', TX '.$options['address-zip'];?>
-				<img src="http://maps.google.com/maps/api/staticmap?size=175x101&amp;markers=size:mid%7Ccolor:blue%7Clabel:Office%7C<?php echo urlencode($mapaddress); ?>&amp;sensor=false" height="101" width="175" alt="Map to office" />
+				<?php 
+				
+				$mapaddress=$options['address-street1'].' '.$options['address-street2'].' '.$options['address-city'].', TX '.$options['address-zip'];
+				$map_image = ($options['map-img']=='' ? 'http://maps.google.com/maps/api/staticmap?size=175x101&amp;markers=size:mid%7Ccolor:blue%7Clabel:Office%7C'.urlencode($mapaddress).'&amp;sensor=false' : $options['map-img']);
+				
+				
+				?>
+				<img src="<?php echo $map_image; ?>" height="101" width="175" alt="Map to office" />
 				<ul>
 				<?php
 				if (is_array($options)) {
+					if($options['hours']<>'') {
+						echo '<li>'.$options['hours'].'</li>';
+					}
 					if(isset($options['address-street1'])) {
 						echo '<li>'.$options['address-street1'];
 						if($options['address-street2']<>'')
 							echo '<br />'.$options['address-street2'];
 						echo '<br />'.$options['address-city'].', TX '.$options['address-zip'].'</li>';
+					}
+					if($options['address-mail-street1']<>'') {
+						echo '<li>'.$options['address-mail-street1'];
+						if($options['address-mail-street2']<>'')
+							echo '<br />'.$options['address-mail-street2'];
+						echo '<br />'.$options['address-mail-city'].', TX '.$options['address-mail-zip'].'</li>';
 					}
 					if($options['phone']<>'')
 						echo '<li>Phone: '.$options['phone'].'</li>';
