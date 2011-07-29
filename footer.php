ce52d87db6fa52b9f3081df2c57deecd5d199a60
Travis Ward <travis@travisward.com>
2011-07-29 11:19:49 -0500

    added map link override and passwor dlogout button

--- footer.php
+++ footer.php
@@ -130,16 +130,17 @@
 				$mapaddress=$options['address-street1'].' '.$options['address-street2'].' '.$options['address-city'].', TX '.$options['address-zip'];
 				$map_image = ($options['map-img']=='' ? 'http://maps.google.com/maps/api/staticmap?size=175x101&amp;markers=size:mid%7Ccolor:blue%7Clabel:Office%7C'.urlencode($mapaddress).'&amp;sensor=false' : $options['map-img']);
 				
+				$map_link = ($options['map-link']=='' ? 'http://maps.google.com/?q='.urlencode($mapaddress).'&amp;markers=size:mid%7Ccolor:blue%7Clabel:Office&amp;sensor=false' : $options['map-link']);
 				
 				?>
-				<img src="<?php echo $map_image; ?>" height="101" width="175" alt="Map to office" />
+				<a href="<?php echo $map_link; ?>"><img src="<?php echo $map_image; ?>" height="101" width="175" alt="Map to office" /></a>
 				<ul>
 				<?php
 				if (is_array($options)) {
 					if($options['hours']<>'') {
 						echo '<li>'.$options['hours'].'</li>';
 					}
-					if(isset($options['address-street1'])) {
+					if($options['address-mail-street1']<>'') {
 						echo '<li>'.$options['address-street1'];
 						if($options['address-street2']<>'')
 							echo '<br />'.$options['address-street2'];
