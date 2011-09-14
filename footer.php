9c8d1570abd673e7e95ff9b2ed98fa66f3e8aad3
Travis Ward <travis@travisward.com>
2011-09-14 15:51:33 -0500

    suzanne d made a content update

--- footer.php
+++ footer.php
@@ -416,7 +416,7 @@
 			<div class="agrilife-bookstore">			
 			<h4>AgriLife Bookstore</h4>
 				<a href="https://agrilifebookstore.org/"><img src="<?php bloginfo( 'template_directory' ); ?>/images/bookstore-books.png" alt="Image of books for the AgriLife Bookstore" /></a>
-				<p>The Texas AgriLife Extension Bookstore offers educational publications, CDs and videos on topics related to agriculture, 4-H and youth development.</p>				
+				<p>AgriLife Extension’s online Bookstore offers educational information and resources related to our many areas of expertise and programming; from agriculture, horticulture, and natural resources to nutrition, wellness for families and youth, and much more.</p>				
 			</div><!-- .agrilife-bookstore -->			
 		</div><!-- #agrilife-bookstore -->
 						
