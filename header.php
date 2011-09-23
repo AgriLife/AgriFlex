5744ec23ea48bb739e840e649bbe2a3bcde28223
Travis Ward <travis@travisward.com>
2011-09-23 15:42:14 -0500

    new default menu

--- header.php
+++ header.php
@@ -153,8 +153,21 @@
 			<nav id="access" role="navigation">		
 			  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
 				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'agriflex' ); ?>"><?php _e( 'Skip to content', 'agriflex' ); ?></a></div>
+				
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>				<div class="clear"></div>
+				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary','fallback_cb' => agrilife_default_menu() ) ); ?>				
+				<div class="clear"></div>
+				
+				<?php 
+				function agrilife_default_menu() {
+					// This mess is to mimic existing DOM structures
+					echo '<div class="menu-header">';
+					$default_menu = wp_page_menu( array( 'show_home' => 'Blog', 'sort_column' => 'menu_order', 'echo' => false ) );;
+					$default_menu = str_replace('<ul>', '<ul id="menu-main-site-navigation" class="sf-menu menu">', $default_menu);
+					$default_menu = str_replace('title="Home"', 'title="home"', $default_menu);  // WP defaults to "Home" on "home"
+					echo $default_menu;
+					echo '</div>';
+				}?>
 			</nav><!-- .access -->
 	<div id="content-wrap">		
 		<div class="wrap">	
