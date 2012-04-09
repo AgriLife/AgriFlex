c449486f1fd5f74fc741f20fa8e282ea85a9ecd1
Jason Weaver <indyplanets@gmail.com>
2012-04-09 12:00:26 -0500

    added taxonomy to Staff post type

--- functions.php
+++ functions.php
@@ -737,7 +737,28 @@ if (!$isextensiononly) {
                )
           );
      }
-    
+       // Add new taxonomy, make it hierarchical (like categories)
+     $labels = array(
+          'name' => _x( 'Type', 'taxonomy general name' ),
+          'singular_name' => _x( 'Type', 'taxonomy singular name' ),
+          'search_items' =>  __( 'Search Types' ),
+          'all_items' => __( 'All Types' ),
+          'parent_item' => __( 'Parent Type' ),
+          'parent_item_colon' => __( 'Parent Types:' ),
+          'edit_item' => __( 'Edit Type' ),
+          'update_item' => __( 'Update Type' ),
+          'add_new_item' => __( 'Add New Type' ),
+          'new_item_name' => __( 'New Type Name' ),
+     );     
+
+     register_taxonomy( 'types', array( 'staff' ), array(
+          'hierarchical' => true,
+          'labels' => $labels, /* NOTICE: Here is where the $labels variable is used */
+          'show_ui' => true,
+          'query_var' => true,
+          'rewrite' => false,
+     ));
+  
     
 }
 
