277a79b97e549add086c83e759592359249ef635
Travis Ward <travis@travsiward.com>
2012-04-26 12:15:48 -0500

    tidy up fonts, add taxonomy browser

--- taxonomy-job_category.php
+++ taxonomy-job_category.php
@@ -15,15 +15,7 @@ get_header(); ?>
 				<h1 class="page-title"><?php
 					printf( __( 'Browse: %s', 'agriflex' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 				?></h1>
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '<div class="archive-meta">' . $category_description . '</div>';
-
-				
-				?>
-
-<?php if ( ! have_posts() ) : ?>
+				<?php if ( ! have_posts() ) : ?>
 		<div id="post-0" class="post error404 not-found">
 			<h1 class="entry-title"><?php _e( 'Not Found', 'agriflex' ); ?></h1>
 			<div class="entry-content">
@@ -42,21 +34,24 @@ get_header(); ?>
 				$custom = get_post_custom($post->ID);
 			
 				$job_number     = ($my_meta['job_number']<>'' ? $my_meta['job_number']	: $custom["job_number"][0]);
+				if($job_number=='')
+					$job_number = get_the_ID();
 				$agency 		= ($my_meta['agency']<>'' ? $my_meta['agency'] 			: $custom["agency"][0]);
 				$location		= ($my_meta['location']<>'' ? $my_meta['location'] 		: $custom["location"][0]);
-				$type			= ($my_meta['type'] <>'' ? $my_meta['type']				: $custom["classification"][0]);
-			
-				?>
-			<li class="job-listing-item">
-				<a class="job-listing-link" href="<?php the_permalink(); ?>">
-				<div class="role">
-					<h2 class="job-title" title="<?php the_title(); ?>"><?php the_title(); ?></h2>
-					<h3 class="job-agency"><?php echo $agency; ?></h3>
-					<p class="job-location location"><?php echo $location; ?></p>
-					<p class="job-type type"><?php echo $type; ?></p>
-				</div>
-				</a>
-			</li>
+					?>
+				<li class="job-listing-item">
+					<a class="job-listing-link" href="<?php the_permalink(); ?>">
+					<div class="role">
+						<h2 class="job-title" title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+						<h3 class="job-agency"><?php echo $agency; ?></h3>
+						<h4 class="job-posted-date">Posted: <?php echo get_the_date('M-d-y'); ?></h4>
+						<h4 class="job-number">Job: <?php echo $job_number; ?></h4>
+						<p class="job-location location"><?php echo $location; ?></p>
+						<!--<p class="job-type type"></p>-->
+					</div>
+					</a>
+				</li>
+
 			
 		<?php endwhile;?>
 	</ul>
