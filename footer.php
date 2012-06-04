e06366ce00f609a7ba89261f3e97163651962411
Travis Ward <travis@travsiward.com>
2012-06-04 12:59:51 -0500

    fazd tweaks

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
  * @since agriflex 1.0
  */
   GLOBAL $isresearch, $isextension, $iscollege, $istvmdl;
-  GLOBAL $isextensiononly, $isresearchonly, $iscollegeonly, $istvmdlonly;
+  GLOBAL $isextensiononly, $isresearchonly, $iscollegeonly, $istvmdlonly, $isfazd;
   GLOBAL $isextension4h, $isextensioncounty, $isextensioncountytce, $isextensionmg, $isextensionmn;
   GLOBAL $options;
 ?>
@@ -23,32 +23,61 @@
 
 // Column 1-3
 if($iscollegeonly) : 
-	include("includes/footer/college.inc.php"); 
+	include("includes/footer/college.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php"); 
 elseif($isresearchonly) : 
-	include("includes/footer/research.inc.php"); 
+	include("includes/footer/research.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php");  
 elseif($istvmdlonly) : 
 	include("includes/footer/tvmdl.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php"); 
 elseif($isfazd) : 
-	include("includes/footer/fazd.inc.php");	
+	include("includes/footer/fazd.inc.php");
+	// Column 4
+	//include("includes/footer/contact.inc.php");  
+	// Column 5
+	//include("includes/footer/bookstore.inc.php"); 	
 elseif($isextensiononly && $isextension4h) : 
 	include("includes/footer/4h.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php"); 
 elseif($isextensiononly && $isextensionmg) : 
 	include("includes/footer/txmg.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php"); 
 elseif($isextensiononly && $isextensionmn) : 
 	include("includes/footer/txmn.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php"); 
 elseif($isextensiononly) : 
 	include("includes/footer/extension.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php"); 
 else : 
 	// Multi-agency 
 	include("includes/footer/multi.inc.php");
+	// Column 4
+	include("includes/footer/contact.inc.php");  
+	// Column 5
+	include("includes/footer/bookstore.inc.php"); 
 endif;
-
-// Column 4
-include("includes/footer/contact.inc.php");  
-
-// Column 5
-include("includes/footer/bookstore.inc.php"); 
-
 ?>	
 		</div><!-- .wrap -->		
 	</footer><!-- #footer -->
