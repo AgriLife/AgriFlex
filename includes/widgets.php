95060f1c25592bfd368f5c8e5638277099c09c2e
J. Aaron Eaton <aaron@channeleaton.com>
2012-08-16 11:36:39 -0500

    Added initial widget code

--- includes/widgets.php
+++ includes/widgets.php
@@ -570,4 +570,84 @@ class Category_Widget extends WP_Widget {
 	}
 
 }
+
+/**
+ * Adds Social Media Widget
+ *
+ * Allows users to input usernames from various social media outlets
+ */
+
+class AgriLife_Social_Media_Icons extents WP_Widget {
+
+  /**
+   * Register widget with WordPress
+   */
+  public function __construct() {
+    parent::__construct(
+      'social_media', // Base ID
+      'Foo_Widget', // Name
+      array('description' => __('Add social media icons', 'text_domain'), ) // Args
+    );
+  }
+
+  /**
+   * Front-end display of widget.
+   *
+   * @see WP_Widget::widget()
+   *
+   * @param array $args     Widget arguments.
+   * @param array $instance Saved values from database.
+   */
+  public function widget( $args, $instance ) {
+    extract( $args );
+    $title = apply_filters( 'widget_title', $instance['title'] );
+
+    echo $before_widget;
+    if( ! empty( $title ) )
+        echo $before_title . $title . $after_title;
+    echo __( 'Hello, World!', 'text_domain' );
+    echo $after_widget;
+  }
+
+  /**
+   * Sanitize widget form values as they are saved.
+   *
+   * @see WP_Widget::update()
+   *
+   * @param array $new_instance   Values just sent to be saved.
+   * @param array $old_instance   Previously saved values from database.
+   *
+   * @return array Updated safe values to be saved.
+   */
+  public function update( $new_instance, $old_instance ) {
+    $instance = array();
+    $instance['title'] = strip_tags( $new_instance['title'] );
+
+    return $instance;
+  }
+
+  /**
+   * Back-end widget form
+   *
+   * @see WP_Widget::form()
+   *
+   * @param array $instance   Previously saved values from database
+   */
+  public function form( $instance ) {
+    if ( isset( $instance['title'] ) ) {
+      $title = $instance['title'];
+    }
+    else {
+      $title = __( 'New Title', 'text_domain' );
+    }
+    ?>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' _; ?>)" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <?php
+  }
+} // class SocialMediaIcons
+
+register_widget('AgriLife_Social_Media_Icons');
 ?>
