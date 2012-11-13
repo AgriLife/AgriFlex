ed99bc2d570f23c2cf6c81502782b47cc065d758
J. Aaron Eaton <aaron@channeleaton.com>
2012-11-13 11:11:06 -0600

    Cleaned up 404.php

--- 404.php
+++ 404.php
@@ -6,25 +6,26 @@
  * @subpackage agriflex
  * @since agriflex 1.0
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
 
-		<div id="wrap">
-			<div id="content" role="main">
+<div id="wrap">
+  <div id="content" role="main">
 
-			<div id="post-0" class="post error404 not-found">
-				<h1 class="entry-title"><?php _e( 'Not Found', 'agriflex' ); ?></h1>
-				<div class="entry-content">
-					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'agriflex' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			</div><!-- #post-0 -->
+    <div id="post-0" class="post error404 not-found">
+      <h1 class="entry-title"><?php _e( 'Not Found', 'agriflex' ); ?></h1>
+      <div class="entry-content">
+        <p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'agriflex' ); ?></p>
+        <?php get_search_form(); ?>
+      </div><!-- .entry-content -->
+    </div><!-- #post-0 -->
 
-		</div><!-- #content -->
-	</div><!-- #container -->
-	<script type="text/javascript">
-		// focus on search field after it has loaded
-		document.getElementById('s') && document.getElementById('s').focus();
-	</script>
+  </div><!-- #content -->
+</div><!-- #container -->
+<script type="text/javascript">
+// focus on search field after it has loaded
+document.getElementById('s') && document.getElementById('s').focus();
+</script>
 
 <?php get_footer(); ?>  
