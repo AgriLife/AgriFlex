7269e34e5640dc3055df9a263cfde2f2a4f40895
J. Aaron Eaton <aaron@channeleaton.com>
2012-11-13 14:39:09 -0600

    404 and no results combined.

--- loop.php
+++ loop.php
@@ -24,13 +24,7 @@
 
 <?php /* If there are no posts to display, such as an empty archive page */ ?>
 <?php if ( ! have_posts() ) : ?>
-	<div id="post-0" class="post error404 not-found">
-		<h1 class="entry-title"><?php _e( 'Not Found', 'agriflex' ); ?></h1>
-		<div class="entry-content">
-			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'agriflex' ); ?></p>
-			<?php get_search_form(); ?>
-		</div><!-- .entry-content -->
-	</div><!-- #post-0 -->
+  <?php get_template_part( '404', 'index' ); ?>
 <?php endif; ?>
 
 <?php
