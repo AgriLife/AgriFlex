fa9ab50f0ace14ad7f93d7999f95509b6b82d8b7
J. Aaron Eaton <aaron@channeleaton.com>
2012-11-14 15:14:49 -0600

    Cleaned up single.php

--- single.php
+++ single.php
@@ -2,30 +2,28 @@
 /**
  * The Template for displaying all single posts.
  *
- * @package WordPress
- * @subpackage agriflex
- * @since agriflex 1.0
+ * @package AgriFlex
+ * @since AgriFlex 1.0
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
 
-		<div id="wrap">
-			<div id="content" role="main">
+<div id="wrap">
+  <div id="content" role="main">
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'single' ); ?>
-				
-				<nav id="nav-below" class="navigation">
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'agriflex' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'agriflex' ) . '</span>' ); ?></div>
-				</nav><!-- #nav-below -->
+      <?php get_template_part( 'content', 'single' ); ?>
 
-				<?php comments_template( '', true ); ?>
+      <?php agriflex_content_nav( 'nav-below' ); ?>
 
-<?php endwhile; // end of the loop. ?>
-			</div><!-- #content -->
-		</div><!-- #wrap -->
+      <?php comments_template( '', true ); ?>
+
+    <?php endwhile; // end of the loop. ?>
+
+  </div><!-- #content -->
+</div><!-- #wrap -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
