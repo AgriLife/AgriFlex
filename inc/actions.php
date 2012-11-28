7c8ec1964a481645489d3aec1ec35c2d7676c2db
J. Aaron Eaton <aaron@channeleaton.com>
2012-11-28 15:47:57 -0600

    Rearranging the furniture

--- inc/actions.php
+++ inc/actions.php
@@ -7,4 +7,59 @@
  * @since AgriFlex 2.0
  */
 
+/**
+ * Add asynchronous google analytics code. Insert the custom account code
+ * if available.
+ *
+ * @since AgriFlex 1.0
+ */
+add_action('wp_head','agriflex_analytics_code',0);
+function agriflex_analytics_code() {
 
+  global $options;
+
+  if( !is_admin() ) : ?>
+    <script type="text/javascript">//<![CDATA[
+    // Google Analytics asynchronous
+    var _gaq = _gaq || [];
+    <?php if( $options['extension_type'] == 2 ||
+              $options['extension_type'] == 3 ) : ?>
+      _gaq.push(['_setAccount','UA-7414081-1']);      //county-co
+      _gaq.push(['_trackPageview'],['_trackPageLoadTime']);
+    <?php endif; ?>
+
+    <?php if( $options['googleAnalytics']<>'' ) {
+      echo "_gaq.push(['_setAccount','" . $options['googleAnalytics'] . "']); //local \n";
+      echo "_gaq.push(['_trackPageview'],['_trackPageLoadTime']);\n";
+    }
+    ?>
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    //]] >
+    </script>
+  <?php
+  endif;
+} // agriflex_analytics_code    
+
+/**
+ * Removes the default styles that are packaged with the Recent Comments widget.
+ *
+ * To override this in a child theme, remove the filter and optionally add your
+ * own function tied to the widgets_init action hook.
+ *
+ * @since AgriFlex 1.0
+ * @global $wp_widget_factory
+ */
+add_action( 'widgets_init', 'agriflex_remove_recent_comments_style' );
+function agriflex_remove_recent_comments_style() {
+
+  global $wp_widget_factory;
+
+  remove_action( 'wp_head',
+    array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'],
+    'recent_comments_style' ) );
+
+}
