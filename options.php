3fcb13f62ee2431e167c40f00d1c9b406fb578d2
J. Aaron Eaton <aaron@channeleaton.com>
2012-12-12 16:06:28 -0600

    Added all fields

--- options.php
+++ options.php
@@ -27,6 +27,30 @@ function optionsframework_option_name() {
 
 function optionsframework_options() {
 
+  // Site title selections
+  $site_title_array = array(
+    0 => 'Site Title (text only)',
+    1 => 'Site Title and Small Logo',
+    2 => 'Custom Image Header'
+  );
+
+  $agency_array = array(
+    'research' => 'Research',
+    'extension' => 'Extension',
+    'college'   => 'College',
+    'tvmdl'     => 'TVMDL'
+  );
+
+  $ext_array = array(
+    'typical' => 'Typical',
+    '4h'      => '4-H',
+    'county'  => 'County Office',
+    'tce'     => 'County TCE Office',
+    'mg'      => 'Master Gardener Chapter',
+    'mn'      => 'Master Naturalist Chapter',
+    'sg'      => 'Sea Grant'
+  );
+
 	// Test data
 	$test_array = array(
 		'one' => __('One', 'options_framework_theme'),
@@ -103,187 +127,198 @@ function optionsframework_options() {
 	$options = array();
 
 	$options[] = array(
-		'name' => __('Basic Settings', 'options_framework_theme'),
-		'type' => 'heading');
+		'name' => __( 'Appearance', 'options_framework_theme' ),
+    'type' => 'heading'
+  );
 
 	$options[] = array(
-		'name' => __('Input Text Mini', 'options_framework_theme'),
-		'desc' => __('A mini text input field.', 'options_framework_theme'),
-		'id' => 'example_text_mini',
-		'std' => 'Default',
-		'class' => 'mini',
-		'type' => 'text');
-
-	$options[] = array(
-		'name' => __('Input Text', 'options_framework_theme'),
-		'desc' => __('A text input field.', 'options_framework_theme'),
-		'id' => 'example_text',
-		'std' => 'Default Value',
-		'type' => 'text');
-
-	$options[] = array(
-		'name' => __('Textarea', 'options_framework_theme'),
-		'desc' => __('Textarea description.', 'options_framework_theme'),
-		'id' => 'example_textarea',
-		'std' => 'Default Text',
-		'type' => 'textarea');
-
-	$options[] = array(
-		'name' => __('Input Select Small', 'options_framework_theme'),
-		'desc' => __('Small Select Box.', 'options_framework_theme'),
-		'id' => 'example_select',
-		'std' => 'three',
-		'type' => 'select',
-		'class' => 'mini', //mini, tiny, small
-		'options' => $test_array);
-
-	$options[] = array(
-		'name' => __('Input Select Wide', 'options_framework_theme'),
-		'desc' => __('A wider select box.', 'options_framework_theme'),
-		'id' => 'example_select_wide',
-		'std' => 'two',
-		'type' => 'select',
-		'options' => $test_array);
-
-	$options[] = array(
-		'name' => __('Select a Category', 'options_framework_theme'),
-		'desc' => __('Passed an array of categories with cat_ID and cat_name', 'options_framework_theme'),
-		'id' => 'example_select_categories',
-		'type' => 'select',
-		'options' => $options_categories);
-	
-	if ($options_tags) {
-	$options[] = array(
-		'name' => __('Select a Tag', 'options_check'),
-		'desc' => __('Passed an array of tags with term_id and term_name', 'options_check'),
-		'id' => 'example_select_tags',
-		'type' => 'select',
-		'options' => $options_tags);
-	}
-
-	$options[] = array(
-		'name' => __('Select a Page', 'options_framework_theme'),
-		'desc' => __('Passed an pages with ID and post_title', 'options_framework_theme'),
-		'id' => 'example_select_pages',
-		'type' => 'select',
-		'options' => $options_pages);
-
-	$options[] = array(
-		'name' => __('Input Radio (one)', 'options_framework_theme'),
-		'desc' => __('Radio select with default options "one".', 'options_framework_theme'),
-		'id' => 'example_radio',
-		'std' => 'one',
+		'name' => __('Site Title Style', 'options_framework_theme'),
+		'desc' => __('Select your site title style', 'options_framework_theme'),
+		'id' => 'site-title',
+		'std' => 0,
 		'type' => 'radio',
-		'options' => $test_array);
+		'options' => $site_title_array);
 
-	$options[] = array(
-		'name' => __('Example Info', 'options_framework_theme'),
-		'desc' => __('This is just some example information you can put in the panel.', 'options_framework_theme'),
-		'type' => 'info');
+  $options[] = array(
+    'name' => __( 'Custom Site Logo', 'options_framework_theme' ),
+    'desc' => __( 'Upload your custom logo', 'options_framework_theme' ),
+    'id'   => 'custom-site-logo',
+    'type' => 'upload',
+  );
 
-	$options[] = array(
-		'name' => __('Input Checkbox', 'options_framework_theme'),
-		'desc' => __('Example checkbox, defaults to true.', 'options_framework_theme'),
-		'id' => 'example_checkbox',
-		'std' => '1',
-		'type' => 'checkbox');
+  $options[] = array(
+    'name' => __( 'Minimal Header', 'options_framework_theme' ),
+    'desc' => __( 'Removes agency logos', 'options_framework_theme' ),
+    'id'   => 'minimal-header',
+    'std'  => 0,
+    'type' => 'checkbox',
+  );
 
-	$options[] = array(
-		'name' => __('Advanced Settings', 'options_framework_theme'),
-		'type' => 'heading');
+  $options[] = array(
+    'name' => __( 'Minimal Footer', 'options_framework_theme' ),
+    'desc' => __( 'Shows only the required links', 'options_framework_theme' ),
+    'id'   => 'minimal-footer',
+    'std'  => 0,
+    'type' => 'checkbox',
+  );
 
+  // Contact Information Tab
 	$options[] = array(
-		'name' => __('Check to Show a Hidden Text Input', 'options_framework_theme'),
-		'desc' => __('Click here and see what happens.', 'options_framework_theme'),
-		'id' => 'example_showhidden',
-		'type' => 'checkbox');
-		
-	$options[] = array(
-		'name' => __('Hidden Text Input', 'options_framework_theme'),
-		'desc' => __('This option is hidden unless activated by a checkbox click.', 'options_framework_theme'),
-		'id' => 'example_text_hidden',
-		'std' => 'Hello',
-		'class' => 'hidden',
-		'type' => 'text');
+		'name' => __( 'Contact Information', 'options_framework_theme' ),
+    'type' => 'heading'
+  );
 
-	$options[] = array(
-		'name' => __('Uploader Test', 'options_framework_theme'),
-		'desc' => __('This creates a full size uploader that previews the image.', 'options_framework_theme'),
-		'id' => 'example_uploader',
-		'type' => 'upload');
+  $options[] = array(
+    'name' => __( 'Phone Number', 'options_framework_theme' ),
+    'desc' => __( 'Ex. 979-999-7777', 'options_framework_theme' ),
+    'id'   => 'phone',
+    'std'  => '',
+    'type' => 'text'
+  );
 
-	$options[] = array(
-		'name' => "Example Image Selector",
-		'desc' => "Images for layout.",
-		'id' => "example_images",
-		'std' => "2c-l-fixed",
-		'type' => "images",
-		'options' => array(
-			'1col-fixed' => $imagepath . '1col.png',
-			'2c-l-fixed' => $imagepath . '2cl.png',
-			'2c-r-fixed' => $imagepath . '2cr.png')
-	);
+  $options[] = array(
+    'name' => __( 'Fax Number', 'options_framework_theme' ),
+    'desc' => __( 'Ex. 979-999-7777', 'options_framework_theme' ),
+    'id'   => 'fax',
+    'std'  => '',
+    'type' => 'text'
+  );
 
-	$options[] = array(
-		'name' =>  __('Example Background', 'options_framework_theme'),
-		'desc' => __('Change the background CSS.', 'options_framework_theme'),
-		'id' => 'example_background',
-		'std' => $background_defaults,
-		'type' => 'background' );
+  $options[] = array(
+    'name' => __( 'Email Address (public)', 'options_framework_theme' ),
+    'desc' => __( 'Ex. example@tamu.edu', 'options_framework_theme' ),
+    'id'   => 'email',
+    'std'  => '',
+    'type' => 'text'
+  );
 
-	$options[] = array(
-		'name' => __('Multicheck', 'options_framework_theme'),
-		'desc' => __('Multicheck description.', 'options_framework_theme'),
-		'id' => 'example_multicheck',
-		'std' => $multicheck_defaults, // These items get checked by default
-		'type' => 'multicheck',
-		'options' => $multicheck_array);
+  $options[] = array(
+    'name' => __( 'Hours of Operation', 'options_framework_theme' ),
+    'desc' => __( 'Ex. Mon-Fri 8:00am-5:00pm', 'options_framework_theme' ),
+    'id'   => 'hours',
+    'std'  => '',
+    'type' => 'text'
+  );
 
-	$options[] = array(
-		'name' => __('Colorpicker', 'options_framework_theme'),
-		'desc' => __('No color selected by default.', 'options_framework_theme'),
-		'id' => 'example_colorpicker',
-		'std' => '',
-		'type' => 'color' );
-		
-	$options[] = array( 'name' => __('Typography', 'options_framework_theme'),
-		'desc' => __('Example typography.', 'options_framework_theme'),
-		'id' => "example_typography",
-		'std' => $typography_defaults,
-		'type' => 'typography' );
-		
-	$options[] = array(
-		'name' => __('Custom Typography', 'options_framework_theme'),
-		'desc' => __('Custom typography options.', 'options_framework_theme'),
-		'id' => "custom_typography",
-		'std' => $typography_defaults,
-		'type' => 'typography',
-		'options' => $typography_options );
+  $options[] = array(
+    'name' => __( 'Physical Address', 'options_framework_theme' ),
+    'desc' => __( 'Not a P.O. Box', 'options_framework_theme' ),
+    'id'   => 'phys-addr',
+    'type' => 'info'
+  );
 
-	$options[] = array(
-		'name' => __('Text Editor', 'options_framework_theme'),
-		'type' => 'heading' );
+  $options[] = array(
+    'name' => __( 'Street 1', 'options_framework_theme' ),
+    'id'   => 'p-street-1',
+    'std'  => '600 John Kimbrough Blvd.',
+    'type' => 'text'
+  );
 
-	/**
-	 * For $settings options see:
-	 * http://codex.wordpress.org/Function_Reference/wp_editor
-	 *
-	 * 'media_buttons' are not supported as there is no post to attach items to
-	 * 'textarea_name' is set by the 'id' you choose
-	 */
+  $options[] = array(
+    'name' => __( 'Street 2', 'options_framework_theme' ),
+    'id'   => 'p-street-2',
+    'std'  => '',
+    'type' => 'text'
+  );
 
-	$wp_editor_settings = array(
-		'wpautop' => true, // Default
-		'textarea_rows' => 5,
-		'tinymce' => array( 'plugins' => 'wordpress' )
-	);
-	
-	$options[] = array(
-		'name' => __('Default Text Editor', 'options_framework_theme'),
-		'desc' => sprintf( __( 'You can also pass settings to the editor.  Read more about wp_editor in <a href="%1$s" target="_blank">the WordPress codex</a>', 'options_framework_theme' ), 'http://codex.wordpress.org/Function_Reference/wp_editor' ),
-		'id' => 'example_editor',
-		'type' => 'editor',
-		'settings' => $wp_editor_settings );
+  $options[] = array(
+    'name' => __( 'City', 'options_framework_theme' ),
+    'id'   => 'p-city',
+    'std'  => 'College Station',
+    'type' => 'text'
+  );
+
+  $options[] = array(
+    'name' => __( 'Zip', 'options_framework_theme' ),
+    'id'   => 'p-zip',
+    'std'  => '77843',
+    'type' => 'text'
+  );
+
+  $options[] = array(
+    'name' => __( 'Mailing Address', 'options_framework_theme' ),
+    'id'   => 'mail-addr',
+    'type' => 'info'
+  );
+
+  $options[] = array(
+    'name' => __( 'Street 1', 'options_framework_theme' ),
+    'id'   => 'm-street-1',
+    'std'  => '',
+    'type' => 'text'
+  );
+
+  $options[] = array(
+    'name' => __( 'Street 2', 'options_framework_theme' ),
+    'id'   => 'm-street-2',
+    'std'  => '',
+    'type' => 'text'
+  );
+
+  $options[] = array(
+    'name' => __( 'City', 'options_framework_theme' ),
+    'id'   => 'm-city',
+    'std'  => '',
+    'type' => 'text'
+  );
+
+  $options[] = array(
+    'name' => __( 'Zip', 'options_framework_theme' ),
+    'id'   => 'm-zip',
+    'std'  => '',
+    'type' => 'text'
+  );
+
+  // Social/Web Services Tab
+  $options[] = array(
+    'name' => __( 'Social/Web Services', 'options_framework_theme' ),
+    'type' => 'heading'
+  );
+
+  $options[] = array(
+    'name' => __( 'Google Analytics', 'options_framework_theme' ),
+    'desc' => __( 'Ex. UA-XXXXX-2', 'options_framework_theme' ),
+    'id'   => 'g-analytics',
+    'std'  => '',
+    'type' => 'text'
+  );
+
+  $options[] = array(
+    'name' => __( 'FeedBurner Feed Address', 'options_framework_theme' ),
+    'desc' => __( 'Ex. http://feeds.feedburner.com/AgriLife', 'options_framework_theme' ),
+    'id'   => 'feedburner',
+    'std'  => '',
+    'type' => 'text'
+  );
+
+  // Agency Affilliation Tab
+  $options[] = array(
+    'name' => __('Agency Affiliation', 'options_framework_theme' ),
+    'type' => 'heading'
+  );
+
+  $options[] = array(
+    'name' => __( 'Agency Selection', 'options_framework_theme' ),
+    'desc' => __( 'Select all that apply', 'options_framework_theme' ),
+    'id'   => 'agency-top',
+    'type' => 'multicheck',
+    'options' => $agency_array
+  );
+
+  $options[] = array(
+    'name' => __( 'Extension Type', 'options_framework_theme' ),
+    'id'   => 'ext-type',
+    'std'  => 'typical',
+    'type' => 'radio',
+    'options' => $ext_array
+  );
+
+  $options[] = array(
+    'name' => __( 'Additional Agency Logos', 'options_framework_theme' ),
+    'desc' => __( 'Upload your custom logo. Must be 45px in height.', 'options_framework_theme' ),
+    'id'   => 'custom-agency-logo',
+    'type' => 'upload',
+  );
 
 	return $options;
 }
