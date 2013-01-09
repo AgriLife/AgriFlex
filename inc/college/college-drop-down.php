0eb3be874ad46835499eca4e797c5e5cad83bc64
J. Aaron Eaton <aaron@channeleaton.com>
2013-01-09 16:32:28 -0600

    Cleaned up college-drop-down

--- inc/college/college-drop-down.php
+++ inc/college/college-drop-down.php
@@ -3,65 +3,67 @@
 		<div class="contents">
 			<div class="drop-section-wrap">
 				<div id="top-level-nav">
-				<ul>
-				<li class="top-level-li sub">About
-				<ul>
-					<li><a href="http://aglifesciences.tamu.edu/about-the-college/welcome-from-the-dean/">Welcome From The Dean</a></li>
-					  <li><a href="http://aglifesciences.tamu.edu/about/administration/">Administration</a></li>
-					  <li><a href="http://aglifesciences.tamu.edu/about/accomplishments/accomplishments-awards/">Achievements</a></li>
-					  <li><a href="http://aglifesciences.tamu.edu/about-the-college/strategic-plan-2010-2015/">Strategic Planning</a></li>
-					  <li><a href="http://agrilife.org/college100">College Centennial</a></li>					
-				</ul>
-				</li>
-					<li class="top-level-li sub">Quick Links
-					<ul>
-					<li><a href="http://aggiemap.tamu.edu/">Campus Map</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/departments/">Academics/Departments</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/academics/institutes-centers/">Institutes &amp; Centers</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/about-the-college/college-fact-sheet/">College Fact Sheet</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/contact/">Contacts</a></li>					
-					<li><a href="http://aglifesciences.tamu.edu/news-and-events/">News &amp; Events</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/careers/">Employment</a></li>
-					
-					
-				</ul>
-				</li>
-				<li class="top-level-li sub">Future Students
-				<ul>
-					<li><a href="http://aglifesciences.tamu.edu/careers/by_major/">Undergraduate Degrees</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/academics/grad/grad_table/">Graduate Degrees</a></li>
-					<li><a href="http://admissions.tamu.edu/transfer/">Transfer Guide</a></li>
-					<li><a href="http://admissions.tamu.edu/international/gettingin/waysAdmitted/freshmen.aspx">Internationals</a></li>
-					<li><a href="http://distance.tamu.edu/">Distance Education</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/future/undergrad/">Admissions</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/future/financialaid/">Financial Aid</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/future/undergrad/tours/">Visit Us!</a></li>
-				</ul>
-				</li>
-				 <li class="top-level-li sub">Current Students
-				<ul>
-					<li><a href="http://aglifesciences.tamu.edu/undergraduate-advisors/">Advising</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/forms-and-procedures/">Forms/Procedures</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/clubs-and-organizations/">Student Organization</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/internships/">Internships</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/study-abroad/">Study Abroad</a></li>	
-					<li><a href="http://aglifesciences.tamu.edu/student-faq/">FAQs</a></li>
-					<li><a href="http://jobsforaggies.tamu.edu/">Student Jobs</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/careers/">Careers</a></li>														
-				</ul>
-				</li>
-				 <li class="top-level-li sub">Former Students/Giving
-				<ul>
-					<li><a href="http://aglifesciences.tamu.edu/giving/">It&rsquo;s Time To Give</a></li>
-					<li><a href="http://coadc.tamu.edu/">COADC</a></li>
-					<li><a href="http://www.aggienetwork.com/">Assoc. of Former Students</a></li>
-					<li><a href="http://agrilife.tamu.edu/awards/outstandingalumni/">Alumni Awards</a></li>					
-					<li><a href="http://www.tamu.edu/home/formerstdts/">Resources</a></li>
-					<li><a href="http://aglifesciences.tamu.edu/giving/meet-our-staff/">Contact</a></li>
-				</ul>
-				 </li>
-				</ul>												
-			</div><!-- #top-level-nav -->				
+          <ul>
+            <li class="top-level-li sub">About
+              <ul>
+                <li><a href="http://aglifesciences.tamu.edu/about-the-college/welcome-from-the-dean/">Welcome From The Dean</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/about/administration/">Administration</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/about/accomplishments/accomplishments-awards/">Achievements</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/about-the-college/strategic-plan-2010-2015/">Strategic Planning</a></li>
+                <li><a href="http://agrilife.org/college100">College Centennial</a></li>					
+              </ul>
+            </li>
+
+            <li class="top-level-li sub">Quick Links
+              <ul>
+                <li><a href="http://aggiemap.tamu.edu/">Campus Map</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/departments/">Academics/Departments</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/academics/institutes-centers/">Institutes &amp; Centers</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/about-the-college/college-fact-sheet/">College Fact Sheet</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/contact/">Contacts</a></li>					
+                <li><a href="http://aglifesciences.tamu.edu/news-and-events/">News &amp; Events</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/careers/">Employment</a></li>
+              </ul>
+            </li>
+
+            <li class="top-level-li sub">Future Students
+              <ul>
+                <li><a href="http://aglifesciences.tamu.edu/careers/by_major/">Undergraduate Degrees</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/academics/grad/grad_table/">Graduate Degrees</a></li>
+                <li><a href="http://admissions.tamu.edu/transfer/">Transfer Guide</a></li>
+                <li><a href="http://admissions.tamu.edu/international/gettingin/waysAdmitted/freshmen.aspx">Internationals</a></li>
+                <li><a href="http://distance.tamu.edu/">Distance Education</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/future/undergrad/">Admissions</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/future/financialaid/">Financial Aid</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/future/undergrad/tours/">Visit Us!</a></li>
+              </ul>
+            </li>
+
+            <li class="top-level-li sub">Current Students
+              <ul>
+                <li><a href="http://aglifesciences.tamu.edu/undergraduate-advisors/">Advising</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/forms-and-procedures/">Forms/Procedures</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/clubs-and-organizations/">Student Organization</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/internships/">Internships</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/study-abroad/">Study Abroad</a></li>	
+                <li><a href="http://aglifesciences.tamu.edu/student-faq/">FAQs</a></li>
+                <li><a href="http://jobsforaggies.tamu.edu/">Student Jobs</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/careers/">Careers</a></li>														
+              </ul>
+            </li>
+
+            <li class="top-level-li sub">Former Students/Giving
+              <ul>
+                <li><a href="http://aglifesciences.tamu.edu/giving/">It&rsquo;s Time To Give</a></li>
+                <li><a href="http://coadc.tamu.edu/">COADC</a></li>
+                <li><a href="http://www.aggienetwork.com/">Assoc. of Former Students</a></li>
+                <li><a href="http://agrilife.tamu.edu/awards/outstandingalumni/">Alumni Awards</a></li>					
+                <li><a href="http://www.tamu.edu/home/formerstdts/">Resources</a></li>
+                <li><a href="http://aglifesciences.tamu.edu/giving/meet-our-staff/">Contact</a></li>
+              </ul>
+            </li>
+          </ul>												
+        </div><!-- #top-level-nav -->				
 			</div><!-- .wrap -->
 		</div><!-- .contents -->	
 	</div><!-- .flow -->		
