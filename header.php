da835d2d5ca859d6b2e54e010cfc5d0ff37a7e4f
J. Aaron Eaton <aaron@channeleaton.com>
2013-09-24 16:23:45 -0500

    AGFLEX-10 Forcing IE out of compatibility mode

--- header.php
+++ header.php
@@ -24,6 +24,8 @@
 <!--<![endif]-->
 <head>
 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 
   <meta name="viewport" content="width=device-width,initial-scale=1">
