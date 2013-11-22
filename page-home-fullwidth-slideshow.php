273a1f6abfd37630f38a5865f9bcf4b4e95811f9
J. Aaron Eaton <aaron@channeleaton.com>
2013-11-22 12:28:28 -0600

    COALS-63 Home slider templates now use Soliloquy

--- page-home-fullwidth-slideshow.php
+++ page-home-fullwidth-slideshow.php
@@ -9,50 +9,7 @@
 
 <?php get_header(); ?>
 
-<?php 
-  $my_query = new WP_Query(
-                    array(
-                      'meta_key'  => 'feature-homepage',
-                      'meta_value'=> '1',
-                      'showposts' => '5',
-                      'post_type' => array( 'post', 'page' )
-                    ));
-  $count = 0;
-  ?>
-
-<?php if ( $my_query->have_posts() ) : ?>
-
-  <div class="flex-container fullwidth">
-    <div class="flexslider">
-      <ul class="slides">
-
-      <?php while ($my_query->have_posts()) : $my_query->the_post();
-      global $post;
-      $count++;
-      $feature_title = get_post_meta($post->ID,'feature-title',true);?>
-
-        <li class="feature-item-<?php echo $count;?> flex-slider-item">			
-          <a href="<?php the_permalink();?>">
-            <?php the_post_thumbnail('featured');?>
-          </a>
-          <p class="flex-caption">
-            <a href="<?php the_permalink();?>">
-              <?php if($feature_title !== '') : ?>
-                <?php echo $feature_title; ?>
-              <?php else : ?>
-                <?php echo get_the_title(); ?>
-              <?php endif; ?>
-            </a>
-          </p><!-- end .flex-caption -->
-        </li><!-- end .feature-item -->						
-
-      <?php endwhile;  wp_reset_query(); ?>
-
-      </ul>
-    </div><!-- .flexslider -->
-  </div><!-- .flex-container -->
-
-<?php endif;?>
+<?php agriflex_home_slider(); ?>
 
 <div id="wrap">		
   <div id="content" role="main">			
