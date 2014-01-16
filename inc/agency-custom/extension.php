3f576986361330278006b50937912d8a090420ed
J. Aaron Eaton <aaron@channeleaton.com>
2014-01-16 09:15:15 -0600

    Killing whitespace

--- inc/agency-custom/extension.php
+++ inc/agency-custom/extension.php
@@ -161,7 +161,7 @@ function extension_about( $about ) {
 
 	$html = '<h4>About</h4>';
 	$html .= '<a href="http://www.youtube.com/watch?v=q_UsLHl_YDQ"><img src="' . get_template_directory_uri() . '/img/about_video.jpg?v=100" alt="link to Extension about video" /></a>';
-	$html .= '<p>A unique education agency, the Texas A&amp;M AgriLife Extension Service teaches Texans wherever they live, extending research-based knowledge to benefit their families and communities.</p>	';
+	$html .= '<p>A unique education agency, the Texas A&amp;M AgriLife Extension Service teaches Texans wherever they live, extending research-based knowledge to benefit their families and communities.</p>';
 
 	return $html;
 
@@ -184,11 +184,11 @@ function extension_links( $links ) {
 	$html .= '<ul>';
 	$html .= '<li><a href="http://county-tx.tamu.edu/">County Extension Offices</a></li>';
 	$html .= '<li><a href="http://agrilife.tamu.edu/locations-window/#centers">Research and Extension Centers</a></li>';
-	$html .= '      <li><a href="https://agrilifepeople.tamu.edu/">Contact Directory</a></li>';
-	$html .= '<li><a href="http://agrilife.org/today/contact-us/">Media Contacts</a></li>					';
-	$html .= '<li><a href="http://texas4-h.tamu.edu/">Texas 4-H and Youth Dev.</a></li>					';
+	$html .= '<li><a href="https://agrilifepeople.tamu.edu/">Contact Directory</a></li>';
+	$html .= '<li><a href="http://agrilife.org/today/contact-us/">Media Contacts</a></li>';
+	$html .= '<li><a href="http://texas4-h.tamu.edu/">Texas 4-H and Youth Dev.</a></li>';
 	$html .= '<li><a href="http://agrilifeextension.tamu.edu/about/strategyimpact/index.php">Strategic Plan and Impacts</a></li>';
-	$html .= '<li class="last"><a href="https://greatjobs.tamu.edu">Employment Opportunities</a></li>										';
+	$html .= '<li class="last"><a href="https://greatjobs.tamu.edu">Employment Opportunities</a></li>';
 	$html .= '</ul>';
 
 	return $html;
@@ -207,8 +207,9 @@ function hhhh_about( $about ) {
 	$html .= '<li><a href="http://texas4-h.tamu.edu/districts">District Websites</a></li>';
 	$html .= '<li><a href="http://texas4-h.tamu.edu/projects">Projects &amp; Programs</a></li>';
 	$html .= '<li><a href="http://texas4-h.tamu.edu/roundup">Roundup</a></li>';
-	$html .= '<li class="last"><a href="http://texas4-h.tamu.edu/contact">Contact Us</a></li>										';
+	$html .= '<li class="last"><a href="http://texas4-h.tamu.edu/contact">Contact Us</a></li>';
 	$html .= '</ul>';
 
 	return $html;
+
 }
--- inc/agency-custom/txmg.php
+++ inc/agency-custom/txmg.php
@@ -24,12 +24,12 @@ function txmg_about( $about ) {
 
   $html = '<h4>About Us</h4>';
   $html .= '<a href="http://www.txmg.org/"><img src="' . get_template_directory_uri() . '/img/about_txmg.jpg?v=100" alt="link to txmg.org" /></a>';
-  $html .= '<p><a href="http://txmg.org/">Texas Master Gardeners</a> is a volunteer program designed to grow horticultural information throughout the state, town by town. </p>	';
+  $html .= '<p><a href="http://txmg.org/">Texas Master Gardeners</a> is a volunteer program designed to grow horticultural information throughout the state, town by town. </p>';
   $html .= '<ul>';
   $html .= '<li><a href="http://txmg.org/become-a-master-gardener/become-a-texas-master-gardener/">Become a Master Gardener</a></li>';
   $html .= '<li><a href="http://txmg.org/future-events/">Master Gardener Events</a></li>';
-  $html .= '<li><a href="http://txmg.org/contacts/by-county/">Chapter By County</a></li>					';
-  $html .= '<li class="last"><a href="http://txmg.org/contacts/program-coordinator/">Contact Us</a></li>										';
+  $html .= '<li><a href="http://txmg.org/contacts/by-county/">Chapter By County</a></li>';
+  $html .= '<li class="last"><a href="http://txmg.org/contacts/program-coordinator/">Contact Us</a></li>';
   $html .= '</ul>';
 
   return $html;
@@ -53,10 +53,10 @@ function txmg_links( $links ) {
   $html .= '<li><a href="http://county-tx.tamu.edu/">County Extension Offices</a></li>';
   $html .= '<li><a href="http://agrilife.tamu.edu/locations-window/#centers">Research and Extension Centers</a></li>';
   $html .= '<li><a href="http://agdirectory.tamu.edu/">Contact Directory</a></li>';
-  $html .= '<li><a href="http://agrilife.org/today/contact-us/">Media Contacts</a></li>					';
-  $html .= '<li><a href="http://texas4-h.tamu.edu/">Texas 4-H and Youth Dev.</a></li>					';
+  $html .= '<li><a href="http://agrilife.org/today/contact-us/">Media Contacts</a></li>';
+  $html .= '<li><a href="http://texas4-h.tamu.edu/">Texas 4-H and Youth Dev.</a></li>';
   $html .= '<li><a href="http://agrilifeextension.tamu.edu/about/strategyimpact/index.php">Strategic Plans, Impacts and Roadmaps</a></li>';
-  $html .= '<li class="last"><a href="http://agrilifeextension.tamu.edu/careers/index.php">Employment Opportunities</a></li>										';
+  $html .= '<li class="last"><a href="http://agrilifeextension.tamu.edu/careers/index.php">Employment Opportunities</a></li>';
   $html .= '</ul>';
 
   return $html;
