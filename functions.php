98e8380cd043b54913d0ab8fcb97dc951025fee6
Zachary Watkins <watkinza@gmail.com>
2016-09-22 14:31:38 -0500

    Restore calendar navigation for months without events

--- functions.php
+++ functions.php
@@ -196,3 +196,25 @@ require_once ($include_path . 'logout-password-protected-posts/logout.php');
 
 // Define location of Options Framework
 define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/inc/options/' );
+
+if ( class_exists( 'Tribe__Events__Main' ) ) {
+  class ContinualMonthViewPagination {
+      public function __construct() {
+          add_filter( 'tribe_events_the_next_month_link', array( $this, 'next_month' ) );
+          add_filter( 'tribe_events_the_previous_month_link', array( $this, 'previous_month' ) );
+      }
+      public function next_month() {
+          $url = tribe_get_next_month_link();
+          $text = tribe_get_next_month_text();
+          $date = Tribe__Events__Main::instance()->nextMonth( tribe_get_month_view_date() );
+          return '<a data-month="' . $date . '" href="' . $url . '" rel="next">' . $text . ' <span>&raquo;</span></a>';
+      }
+      public function previous_month() {
+          $url = tribe_get_previous_month_link();
+          $text = tribe_get_previous_month_text();
+          $date = Tribe__Events__Main::instance()->previousMonth( tribe_get_month_view_date() );
+          return '<a data-month="' . $date . '" href="' . $url . '" rel="prev"><span>&laquo;</span> ' . $text . ' </a>';
+      }
+  }
+  new ContinualMonthViewPagination;
+}
