1e648cefd804756ed89215fd28129b14077c2a4c
Zachary Watkins <watkinza@gmail.com>
2017-04-28 15:22:31 -0500

    Address javascript errors with theme

--- js/flex-bootstrap.js
+++ js/flex-bootstrap.js
@@ -2,3 +2,7 @@
 jQuery(window).load(function() {
   jQuery(".entry-content").fitVids();
 });
+// Restores the jQuery curCSS method since it was removed in version 1.8
+jQuery.curCSS = function(element, prop, val) {
+    return jQuery(element).css(prop, val);
+};
