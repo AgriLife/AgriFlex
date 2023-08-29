5f8bd366a3ce783dcadb448a2a900e451ae4693a
Jared Leonguerrero <linuxpir8@gmail.com>
2023-08-29 07:24:30 -0500

    Update single.php
    updated deprecated code style *TEST*

--- single.php
+++ single.php
@@ -5,31 +5,43 @@
  * @package AgriFlex
  * @since AgriFlex 1.0
  */
-?>
 
-<?php get_header(); ?>
+// Include header
+get_header();
 
-<div id="wrap">
-  <div id="content" role="main">
+// Initialize main content area
+echo '<div id="wrap">';
+echo '<div id="content" role="main">';
 
-    <!-- Action hook to insert content before the loop starts -->
-    <?php agriflex_before_loop(); ?>
+// Execute functions hooked to 'agriflex_before_loop'
+do_action('agriflex_before_loop');
 
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+// Check if there are posts
+if ( have_posts() ) {
+    // Loop through the posts
+    while ( have_posts() ) {
+        the_post();
+        
+        // Load single post content template
+        get_template_part('content', 'single');
+        
+        // Output navigation below the content
+        do_action('agriflex_content_nav', 'nav-below');
+        
+        // Load the comments template
+        if ( comments_open() || get_comments_number() ) {
+            comments_template('', true);
+        }
+    }
+}
 
-      <?php get_template_part( 'content', 'single' ); ?>
+// Execute functions hooked to 'agriflex_after_loop'
+do_action('agriflex_after_loop');
 
-      <?php agriflex_content_nav( 'nav-below' ); ?>
+// Close main content area
+echo '</div><!-- #content -->';
+echo '</div><!-- #wrap -->';
 
-      <?php comments_template( '', true ); ?>
-
-    <?php endwhile; // end of the loop. ?>
-
-    <!-- Action hook to insert content after the loop ends -->
-    <?php agriflex_after_loop(); ?>
-
-  </div><!-- #content -->
-</div><!-- #wrap -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+// Include sidebar and footer
+get_sidebar();
+get_footer();
